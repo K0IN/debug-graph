524417f44f4df459573b2e848905d7d3415a01a9
K0IN <thisk0in@gmail.com>
2024-07-20 21:31:06 +0200

    Update main.py

--- test_code/main.py
+++ test_code/main.py
@@ -1,7 +1,25 @@
+"""a test module"""
+
+
+class Dummy:
+    """dummy class"""
+
+    def __init__(self):
+        self.a = 1
+        self.b = 2
+
+    def dummy_method(self):
+        """dummy method"""
+        print("This is a dummy method", self.a, self.b)
+
+
 def deep_code():
-    a = 1
+    """i do stuff"""
+    a = {"a": 1, "b": 2, "c": 3}
+    x = Dummy()
+    x.dummy_method()
     b = 2
-    c = a + b
+    c = 1 + b
     d = c ** c
     print("This is a deep code", c, d)
 
@@ -11,11 +29,16 @@ def deep_code():
 
 
 def main():
+    """main code"""
     print("This is a main code")
+    a = 1
+    b = 2
+    c = a + b
     deep_code()
     deep_code()
     deep_code()
 
 
 if __name__ == "__main__":
+    a = 1337
     main()
