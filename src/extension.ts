41dc269e9aeaedccb7351215fa8868ee741b0f35
K0IN <thisk0in@gmail.com>
2024-07-23 22:09:12 +0200

    Refactor extension
    * Add better error logging
    * Add webview restore
    * Add releasing rpc channel
    * Add only one variable for rpc channel
    * Remove unused code / comments

--- src/extension.ts
+++ src/extension.ts
@@ -9,12 +9,16 @@ import { FrontendApi } from './debug/frontend-functions';
 
 let currentFrontendRpcChannel: Comlink.Remote<ComlinkFrontendApi> | undefined = undefined;
 
-async function updateViewWithStackTrace(webview: Webview, context: ExtensionContext) {
-  const result = await getStacktraceInfo();
+async function updateViewWithStackTrace(_webview: Webview, _context: ExtensionContext) {
   try {
-    await currentFrontendRpcChannel?.setStackTrace(result);
+    if (!currentFrontendRpcChannel) {
+      throw new Error("No rpc channel found");
+    }
+    const result = await getStacktraceInfo();
+    await currentFrontendRpcChannel.setStackTrace(result);
   } catch (e: unknown) {
-    window.showErrorMessage("failed to send stacktrace to ui, try agin");
+    // todo remove in release build!
+    window.showErrorMessage("failed to load stacktrace, due to error, please try to open view again. Error: " + e);
   }
 }
 
@@ -40,65 +44,64 @@ export async function activate(context: ExtensionContext) {
     }
   }));
 
-  // todo add stacktrace infos in debug as hover provider
-  //context.subscriptions.push(languages.registerHoverProvider({ scheme: 'file' }, {
-  //  async provideHover(document, position, token) {
-  //    return undefined;
-  //    // return new Hover('I am a hover!');
-  //  },
-  //}));
-
   workspace.onDidChangeConfiguration(async event => {
-    if (event.affectsConfiguration('workbench.colorTheme') && currentFrontendRpcChannel) {
-      const { setTheme } = currentFrontendRpcChannel;
-      try {
-        const theme = await getMonacoTheme();
-        await setTheme(theme);
-      } catch (e) {
-        console.error(e);
-      }
+    if (event.affectsConfiguration('workbench.colorTheme')) {
+      const theme = await getMonacoTheme();
+      await currentFrontendRpcChannel?.setTheme(theme);
     }
   });
 
   commands.registerCommand('call-graph.show-call-graph', async () => {
-    if (!currentPanel?.webview) {
-      currentPanel = createWebview(context);
-      currentFrontendRpcChannel = undefined;
-    } else {
-      currentPanel.reveal();
-    }
+    try {
+      if (!currentPanel?.webview) {
+        currentPanel = createWebview(context);
+        currentFrontendRpcChannel = undefined;
+      } else {
+        currentPanel.reveal();
+      }
 
-    currentPanel.webview.html = getVueFrontendPanelContent(context, currentPanel);
-    Comlink.expose(FrontendApi, getComlinkChannel(currentPanel.webview, context));
+      currentPanel.webview.html = getVueFrontendPanelContent(context, currentPanel);
 
-    currentPanel.onDidDispose(() => {
-      currentPanel = undefined;
+      // wait for loading
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      Comlink.expose(FrontendApi, getComlinkChannel(currentPanel.webview, context));
+
+      currentPanel.onDidDispose(() => {
+        currentPanel = undefined;
+        if (currentFrontendRpcChannel) {
+          currentFrontendRpcChannel[Comlink.releaseProxy]();
+        }
+        currentFrontendRpcChannel = undefined;
+      });
+
+      // release current channel if needed
       if (currentFrontendRpcChannel) {
         currentFrontendRpcChannel[Comlink.releaseProxy]();
+        currentFrontendRpcChannel = undefined;
       }
-      currentFrontendRpcChannel = undefined;
-    });
 
-    // release current channel if needed
-    if (currentFrontendRpcChannel) {
-      currentFrontendRpcChannel[Comlink.releaseProxy]();
-      currentFrontendRpcChannel = undefined;
+      currentFrontendRpcChannel = Comlink.wrap<ComlinkFrontendApi>(getComlinkChannel(currentPanel.webview, context));
+
+      // fire and for get - try to set the theme but don't mind if we cant.
+      getMonacoTheme().then(theme => currentFrontendRpcChannel?.setTheme(theme).catch(e => window.showErrorMessage("failed to set style in stack trace view due to error:" + e)));
+
+
+      if (debug.activeDebugSession) {
+        await updateViewWithStackTrace(currentPanel.webview, context);
+      } else {
+        window.showWarningMessage('No active debug session, the view will automatically update when a debug session is started');
+      }
+    } catch (e: unknown) {
+      window.showErrorMessage("failed, to create panel" + e);
     }
+  });
 
-    currentFrontendRpcChannel = Comlink.wrap<ComlinkFrontendApi>(getComlinkChannel(currentPanel.webview, context));
 
-    const { setTheme } = currentFrontendRpcChannel;
-    try {
-      const theme = await getMonacoTheme();
-      await setTheme(theme);
-    } catch (e) {
-      console.error(e);
-    }
-
-    if (debug.activeDebugSession) {
-      await updateViewWithStackTrace(currentPanel.webview, context);
-    } else {
-      window.showWarningMessage('No active debug session, the view will automatically update when a debug session is started');
+  window.registerWebviewPanelSerializer('graph-visualization', {
+    deserializeWebviewPanel: async function (webviewPanel: WebviewPanel, state: unknown): Promise<void> {
+      currentPanel = webviewPanel;
+      await commands.executeCommand('call-graph.show-call-graph');
     }
   });
 }
