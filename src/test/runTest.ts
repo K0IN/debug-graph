a3773f8021c2e176b7bec6cbef351f9322fc6ffa
K0IN <thisk0in@gmail.com>
2024-07-28 23:10:03 +0200

    Update runTest.ts

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,21 +3,24 @@ import * as path from 'path';
 import { runTests } from '@vscode/test-electron';
 
 async function main() {
-	try {
-		// The folder containing the Extension Manifest package.json
-		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+  try {
+    // The folder containing the Extension Manifest package.json
+    // Passed to `--extensionDevelopmentPath`
+    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.resolve(__dirname, './suite/index');
-
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
-	} catch (err) {
-		console.error('Failed to run tests', err);
-		process.exit(1);
-	}
+    // The path to test runner
+    const extensionTestsPath = path.resolve(__dirname, './suite/index');
+    const launchArgs = [
+      '--disable-extensions',
+      '--install-extension=ms-python.debugpy',
+      '--install-extension=ms-python.python'
+    ];
+    // Download VS Code, unzip it and run the integration test
+    await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs });
+  } catch (err) {
+    console.error('Failed to run tests', err);
+    process.exit(1);
+  }
 }
 
 main();
