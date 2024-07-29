9e2bbde816f4dcd65cd540d53d51f613290891b8
K0IN <thisk0in@gmail.com>
2024-07-29 23:35:26 +0200

    Update python.test.ts

--- src/test/suite/python.test.ts
+++ src/test/suite/python.test.ts
@@ -55,12 +55,7 @@ suite('Test python compatibility', function () {
     // console.log('config = ', config);
     // const onStackItemChanged = new Promise<void>((resolve) => vscode.debug.onDidChangeActiveStackItem(() => resolve()));
     //await vscode.debug.startDebugging(undefined, config);
-    await vscode.commands.executeCommand('workbench.action.debug.start', undefined, {
-      type: 'python',
-      request: 'launch',
-      name: 'Launch Program',
-      program: mainFileUri.fsPath,
-    });
+    await vscode.commands.executeCommand('workbench.action.debug.start');
 
     // await onStackItemChanged;
 
