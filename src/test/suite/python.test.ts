f13fa280fb7f46d94c151e266df6566eb8ef1932
K0IN <thisk0in@gmail.com>
2024-07-30 00:21:30 +0200

    Fix test for local development

--- src/test/suite/python.test.ts
+++ src/test/suite/python.test.ts
@@ -1,6 +1,7 @@
 import * as assert from 'assert';
 import path from 'path';
 import * as vscode from 'vscode';
+import { getStacktraceInfo } from '../../debug/callstack-extractor';
 
 suite('Test python compatibility', function () {
   this.timeout(60_000);
@@ -16,67 +17,37 @@ suite('Test python compatibility', function () {
   });
 
   test('Test Call Stack', async () => {
-
     const extensionDevelopmentPath = path.resolve(__dirname, '../../../test_code/');
     const workspaceFolderPath = path.join(extensionDevelopmentPath, 'python');
     const workspaceTestFilePath = path.join(workspaceFolderPath, 'main.py');
 
     const mainFileUri = vscode.Uri.file(workspaceTestFilePath);
 
-    const folders = vscode.workspace.workspaceFolders;
-
-    const success = vscode.workspace.updateWorkspaceFolders(
-      folders
-        ? folders.length
-        : 0,
-      null, { uri: vscode.Uri.from({ scheme: 'file', path: workspaceFolderPath }), name: 'python_code' });
-
-    assert.ok(success, 'Failed to add workspace folder');
-    // assert.ok(vscode.workspace.workspaceFolders, 'Failed to get workspace folders');
-    // const workspace = vscode.workspace.workspaceFolders?.[0];
-    // assert.ok(workspace, 'Failed to get workspace folder');
-
     const breakpoints = [
       new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(13, 0))),
       new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(38, 0))),
     ];
-
     vscode.debug.addBreakpoints(breakpoints);
 
-
-    // (vscode.workspace.workspaceFolders as any) = [mockWorkspaceFolder];
-    // const config: vscode.DebugConfiguration = {
-    //   type: 'python',
-    //   request: 'launch',
-    //   name: 'Launch Program',
-    //   program: mainFileUri.fsPath,
-    // };
-
-    // console.log('config = ', config);
-    // const onStackItemChanged = new Promise<void>((resolve) => vscode.debug.onDidChangeActiveStackItem(() => resolve()));
-    //await vscode.debug.startDebugging(undefined, config);
-    //await vscode.commands.executeCommand('workbench.action.debug.start');
-
-    console.log('mainFileUri = ', mainFileUri.fsPath);
-
-
-    await vscode.debug.startDebugging(undefined, {
+    const config: vscode.DebugConfiguration = {
       type: 'python',
-      name: 'Python: Current File',
       request: 'launch',
+      name: 'Launch Program',
       program: mainFileUri.fsPath,
-    });
-    // await onStackItemChanged;
+    };
 
-    // const stackTraceInfo = await getStacktraceInfo();
-    // assert.ok(stackTraceInfo, 'Failed to get stack trace info');
-    // // asset only the first and last stack frame for now
-    // assert.equal(stackTraceInfo.length, 4, 'Did not get the expected number of stack frames');
-    // assert.equal(stackTraceInfo[0].locationInCode.startLine, 2, 'Did not get the expected line inside function');
-    // assert.equal(stackTraceInfo[0].fileLocationOffset.startLine, 11, 'Did not get the expected offset in file');
+    const onStackItemChanged = new Promise<void>((resolve) => vscode.debug.onDidChangeActiveStackItem(() => resolve()));
+    await vscode.debug.startDebugging(undefined, config);
+    await onStackItemChanged; // wait for the first stack frame to be available
 
-    // assert.equal(stackTraceInfo[3].locationInCode.startLine, 3, 'Did not get the expected line inside function');
-    // assert.equal(stackTraceInfo[3].fileLocationOffset.startLine, 41, 'Did not get the expected offset in file');
+    const stackTraceInfo = await getStacktraceInfo();
+    assert.ok(stackTraceInfo, 'Failed to get stack trace info');
+    // asset only the first and last stack frame for now
+    assert.equal(stackTraceInfo.length, 4, 'Did not get the expected number of stack frames');
+    assert.equal(stackTraceInfo[0].locationInCode.startLine, 2, 'Did not get the expected line inside function');
+    assert.equal(stackTraceInfo[0].fileLocationOffset.startLine, 11, 'Did not get the expected offset in file');
 
+    assert.equal(stackTraceInfo[3].locationInCode.startLine, 3, 'Did not get the expected line inside function');
+    assert.equal(stackTraceInfo[3].fileLocationOffset.startLine, 41, 'Did not get the expected offset in file');
   });
 });
