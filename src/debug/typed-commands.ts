3d9d51007c5a2067d539d566b49af5d9420aa108
K0IN <thisk0in@gmail.com>
2024-07-31 18:26:29 +0200

    Remove unused await

--- src/debug/typed-commands.ts
+++ src/debug/typed-commands.ts
@@ -1,5 +1,5 @@
 import { DocumentSymbol, Uri, commands } from "vscode";
 
-export async function executeDocumentSymbolProvider(uri: Uri) {
-  return await commands.executeCommand</* SymbolInformation[] |*/ DocumentSymbol[] | undefined>('vscode.executeDocumentSymbolProvider', uri);
+export function executeDocumentSymbolProvider(uri: Uri) {
+  return commands.executeCommand</* SymbolInformation[] |*/ DocumentSymbol[] | undefined>('vscode.executeDocumentSymbolProvider', uri);
 }
