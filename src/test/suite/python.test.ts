531db1dde29c59be2141b536b2713ee70825d284
K0IN <thisk0in@gmail.com>
2024-07-31 18:27:09 +0200

    Use new Helper functions

--- src/test/suite/python.test.ts
+++ src/test/suite/python.test.ts
@@ -3,16 +3,16 @@ import path from 'path';
 import * as vscode from 'vscode';
 import { getStacktraceInfo } from '../../debug/callstack-extractor';
 import { getCurrentValueForPosition } from '../../inspect';
+import { executeWithBreakpoints } from './execute-helper';
+import { validateStacktraceInfo } from './validators';
 
 suite('Test python compatibility', function () {
   this.timeout(60_000);
 
   this.beforeAll(async () => {
     await vscode.commands.executeCommand('workbench.action.closeAllEditors');
-    // install extensions
     await vscode.commands.executeCommand('workbench.extensions.installExtension', 'ms-python.debugpy');
     await vscode.commands.executeCommand('workbench.extensions.installExtension', 'ms-python.python');
-
     await vscode.extensions.getExtension('ms-python.debugpy')!.activate();
     await vscode.extensions.getExtension('ms-python.python')!.activate();
   });
@@ -31,59 +31,95 @@ suite('Test python compatibility', function () {
 
     const mainFileUri = vscode.Uri.file(workspaceTestFilePath);
 
-    const breakpoints = [
-      new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(12, 0))),
-      new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(37, 0))),
-    ];
-    vscode.debug.addBreakpoints(breakpoints);
-
-    const config: vscode.DebugConfiguration = {
+    await executeWithBreakpoints({
       type: 'python',
       request: 'launch',
       name: 'Launch Program',
       program: mainFileUri.fsPath,
-    };
-
-    const onStackItemChanged = new Promise<void>((resolve) => vscode.debug.onDidChangeActiveStackItem(() => resolve()));
-    await vscode.debug.startDebugging(undefined, config);
-    await onStackItemChanged; // wait for the first stack frame to be available
+    }, [
+      new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(12, 0))),
+    ]);
 
     const stackTraceInfo = await getStacktraceInfo();
     assert.ok(stackTraceInfo, 'Failed to get stack trace info');
-    // asset only the first and last stack frame for now
-    assert.equal(stackTraceInfo.length, 4, 'Did not get the expected number of stack frames');
-    assert.equal(stackTraceInfo[0].locationInCode.startLine, 2, 'Did not get the expected line inside function');
-    assert.equal(stackTraceInfo[0].fileLocationOffset.startLine, 11, 'Did not get the expected offset in file');
-
-    assert.equal(stackTraceInfo[3].locationInCode.startLine, 3, 'Did not get the expected line inside function');
-    assert.equal(stackTraceInfo[3].fileLocationOffset.startLine, 41, 'Did not get the expected offset in file');
+    validateStacktraceInfo(stackTraceInfo, [
+      {
+        code: "def dummy_method(self):\r\n        \"\"\"dummy method\"\"\"\r\n        print(\"This is a dummy method\", self.a, self.b)",
+        file: "/e:/src/stacktrace-history/test_code/python/main.py",
+        frameId: 2,
+        language: "python",
+        fileLocationOffset: {
+          startLine: 11,
+          startCharacter: 0,
+        },
+        locationInCode: {
+          startLine: 2,
+          startCharacter: 0,
+        },
+      },
+      {
+        code: "def deep_code():\r\n    \"\"\"i do stuff\"\"\"\r\n    a = {\"a\": 1, \"b\": 2, \"c\": 3}\r\n    x = Dummy()\r\n    x.dummy_method()\r\n    b = 2\r\n    c = 1 + b\r\n    d = c ** c\r\n    print(\"This is a deep code\", c, d)\r\n\r\n    def lol():\r\n        print(\"This is a lol function\")\r\n    lol()",
+        file: "/e:/src/stacktrace-history/test_code/python/main.py",
+        frameId: 3,
+        language: "python",
+        fileLocationOffset: {
+          startLine: 16,
+          startCharacter: 0,
+        },
+        locationInCode: {
+          startLine: 4,
+          startCharacter: 0,
+        },
+      },
+      {
+        code: "def main():\r\n    \"\"\"main code\"\"\"\r\n    print(\"This is a main code\")\r\n    a = 1\r\n    b = 2\r\n    c = a + b\r\n    deep_code()\r\n    deep_code()\r\n    deep_code()",
+        file: "/e:/src/stacktrace-history/test_code/python/main.py",
+        frameId: 4,
+        language: "python",
+        fileLocationOffset: {
+          startLine: 31,
+          startCharacter: 0,
+        },
+        locationInCode: {
+          startLine: 6,
+          startCharacter: 0,
+        },
+      },
+      {
+        code: "\r\nif __name__ == \"__main__\":\r\n    a = 1337\r\n    main()\r\n",
+        file: "/e:/src/stacktrace-history/test_code/python/main.py",
+        frameId: 5,
+        language: "python",
+        fileLocationOffset: {
+          startLine: 41,
+          startCharacter: 0,
+        },
+        locationInCode: {
+          startLine: 3,
+          startCharacter: 0,
+        },
+      },
+    ]);
   });
 
   test('[python] Test Inspect variable', async () => {
     const extensionDevelopmentPath = path.resolve(__dirname, '../../../test_code/');
     const workspaceFolderPath = path.join(extensionDevelopmentPath, 'python');
     const workspaceTestFilePath = path.join(workspaceFolderPath, 'main.py');
-
     const mainFileUri = vscode.Uri.file(workspaceTestFilePath);
 
-    const breakpoints = [
-      new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(18, 0))),
-    ];
-    vscode.debug.addBreakpoints(breakpoints);
-
-    const config: vscode.DebugConfiguration = {
+    await executeWithBreakpoints({
       type: 'python',
       request: 'launch',
       name: 'Launch Program',
       program: mainFileUri.fsPath,
-    };
-
-    const onStackItemChanged = new Promise<void>((resolve) => vscode.debug.onDidChangeActiveStackItem(() => resolve()));
-    await vscode.debug.startDebugging(undefined, config);
-    await onStackItemChanged; // wait for the first stack frame to be available
+    }, [
+      new vscode.SourceBreakpoint(new vscode.Location(mainFileUri, new vscode.Position(18, 0))),
+    ]);
 
     const valuePosition = await getCurrentValueForPosition(mainFileUri, 17, 5, 0);
     assert.ok(valuePosition, 'Failed to get stack trace info');
+    // todo use validateVariableLookup
     assert.equal(valuePosition?.formattedValue, "{'a': 1, 'b': 2, 'c': 3}", 'Did not get the expected value');
     assert.equal(valuePosition?.variableInfo?.length, 6, 'found unexpected number of variables');
   });
